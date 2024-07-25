05845c66df8ff18249487f1e8a6f10e9c45ae92d
Prajapati-ankit <105482209+5655565@users.noreply.github.com>
2024-07-25 22:15:59 +0530

    Class-D

---
+++ TEAM/surbhi/front-end/src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Member extends React.Component{
+    render(){
+        return(
+            // console.log("")
+            <h1>Hello</h1>
+        )
+    }
+}
+export default Member;
