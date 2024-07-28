3561201ecd86c39494923271e08db63b1a9ad20a
MrSurajkar <jainish.surajkar.official@gmail.com>
2024-07-28 10:54:46 +0530

    Login

---
+++ TEAM/jainish/React/my_app/src/components/login/login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './login.css'
+
+export const login = () => {
+  return (
+        <div class="center">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Enter Username" name="username" required/><br/>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Enter Password" name="password" required/><br/>
+            </div>
+            
+                <button type="submit" name="login">Login</button>
+
+            <div class="forget-password">
+                <a href="forget_password.html">Forgot Password?</a>
+            </div>
+            <div class="register">
+                Don't have any account ?<a href=""> Create one </a>
+            </div>
+        </div>
+  )
+}
