7012355a61e17d97a3931ff4747fbfe72e311360
Prajapati-ankit <105482209+5655565@users.noreply.github.com>
2024-07-31 19:10:16 +0530

    LOL

--- TEAM/surbhi/React-Quiz/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import LogoImg from '../assets/quiz-logo.png';
-export function Header(){
-    return(
-    <header>
-        <img src={LogoImg} alt='Quiz logo'/>
-        <h1>React Quiz</h1>
-    </header>
-    );
-}
