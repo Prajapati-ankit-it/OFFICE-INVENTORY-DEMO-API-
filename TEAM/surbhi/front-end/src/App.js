2b95fdfc7c28f25ea257482445634ab489fe407a
surbhijadav <jadavsubhu04@outlook.com>
2024-08-01 17:53:27 +0530

    START

--- TEAM/surbhi/front-end/src/App.js
+++ TEAM/surbhi/front-end/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div classname="App">
-      <header classname="App-header">
-        <img src={logo} classname="App-logo" alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          classname="App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
