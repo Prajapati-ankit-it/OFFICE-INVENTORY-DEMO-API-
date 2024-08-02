2a42ead9bf1afa7f441e151c070e3a3e5e41ce44
MrSurajkar <jainish.surajkar.official@gmail.com>
2024-08-03 00:13:51 +0530

    back-end part 1

--- TEAM/jainish/back-end/server.js
+++ TEAM/jainish/back-end/server.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
 
-PORT = process.env || 8085
+PORT = process.env.PORT || 1000
+
+app.get("/", function(req, res){
+    res.send("<h1> This is the home page.</h1>")
+})
 
 app.listen(PORT, function(){
     console.log(`I am here and the PORT is ${PORT}`)
