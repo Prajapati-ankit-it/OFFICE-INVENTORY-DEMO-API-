cba1c4180a3ff4212868aa48402db41a0d609c24
kavyajadav3107 <kavyajadv@gmail.com>
2024-08-05 20:17:45 +0530

    destructuring obj

--- TEAM/kavy/let/destructuring object.ts
+++ TEAM/kavy/let/destructuring object.ts
@@ -8,3 +8,5 @@ let employee ={
  console.log(f)
  console.log(l)
  console.log(g)
+ // we use desturcturing objects for extracting values from propertis and assigning them to variables means properties to one variable 
+ // and same as array we use it to remove them from properties and assigning them to variable.
